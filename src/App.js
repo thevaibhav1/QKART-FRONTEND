c1d7e60797bf6ba247de8dcf1672d5a25c343afb
CrioUser <crio-user@criodo.com>
2022-10-08 19:18:52 +0530

    “ME_QKART_FRONTEND_V2_MODULE_DEPLOYMENT”

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Checkout from "./components/Checkout"
 import Thanks from "./components/Thanks"
 
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  endpoint: `https://vaibhav-qkart-frontend.herokuapp.com/api/v1`,
 };
 
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App";
 import { SnackbarProvider } from "notistack";
 import { BrowserRouter } from "react-router-dom";
+import theme from "./theme"
 import { ThemeProvider } from "@mui/system";
-import theme from "./theme";
 
+
+// TODO: CRIO_TASK_MODULE_REGISTER - Add Target container ID (refer public/index.html)
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
         <SnackbarProvider
           maxSnack={1}
           anchorOrigin={{
@@ -19,6 +23,8 @@ ReactDOM.render(
         >
           <App />
         </SnackbarProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
-   document.getElementById('')
+  document.getElementById("root")
 );
